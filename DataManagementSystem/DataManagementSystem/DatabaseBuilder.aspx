f1aaf5505f67bfd5e9eedce4dc72a0f4f673864b
Devesh Gupta <devesh.gupta@unthinkable.co>
2020-09-21 20:25:44 +0530

    CRUD Operation Implementation

---
+++ DataManagementSystem/DataManagementSystem/DatabaseBuilder.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DatabaseBuilder.aspx.cs" Inherits="DataManagementSystem.DatabaseBuilder" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table align="Center">
+                <caption>Database System</caption>
+                <tr>
+                    <td>Dept ID:</td>
+                    <td><asp:TextBox ID="txt_DeptName" runat="server" Width="150px"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Dept Name:</td>
+                    <td><asp:Textbox ID="txt_DeptId" runat="server" Width="150px"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Location ID:</td>
+                    <td><asp:Textbox ID="txt_LocationId" runat="server" Width="150px"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><asp:Button ID="btn_Add" runat="server" Text="Add" OnClick="btn_Add_Click" Width="80px"/></td>
+                    <td><asp:Button ID="btn_Save" runat="server" Text="Save" OnClick="btn_Save_Click" Width="77px"/></td>
+                </tr>
+                <tr>
+                    <td><asp:Button ID="btn_Search" runat="server" Text="Search" OnClick="btn_Search_Click" Width="79px"/></td>
+                    <td><asp:Button ID="btn_Update" runat="server" Text="Update" OnClick="btn_Update_Click"/></td>
+                </tr>
+                <tr>
+                    <td><asp:Button ID="btn_Delete" runat="server" Text="Delete" OnClick="btn_Delete_Click" Width="80px"/></td>
+                </tr>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DataManagementSystem/DataManagementSystem/DatabaseBuilder.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataManagementSystem
+{
+    public partial class DatabaseBuilder : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+			txt_DeptId.Focus();
+		}
+
+		SqlConnection con;
+		SqlCommand cmd;
+		public EstablishingConnection()
+		{
+			InitialiseComponent();
+			con = new SqlConnection(ConfiguraionManager.ConnectionStrings["constr"].ConnectionString);
+		}
+		void Reset()
+		{
+			txt_DeptName.Text = txt_DeptId.Text = txt_LocationId = "";
+			txt_DeptId.Focus();
+		}
+		private void btn_Add_Click(object sender, EventArgs e)
+		{
+			Reset();
+			string command = "select max(DeptId) from tbl_Departments";
+			cmd = new SqlCommand(command, con);
+			con.Open();
+			int id = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+			con.Close();
+			txt_DeptId.Text = id.ToString();
+		}
+
+		private void btn_Save_Click(object sender, EventArgs e)
+		{
+			string id = txt_DeptId.Text;
+			string name = txt_DeptName.Text;
+			string loc = txt_LocationId.Text;
+
+			string command = string.Format("insert into tbl_Departments values({0}, '{1}' , {2})", id, name, loc);
+
+			cmd = new SqlCommand(command, con);
+			con.Open();
+			cmd.ExecuteNonQuery();
+			con.Close();
+			Reset();
+		}
+
+		private void btn_Search_Click(object sender, EventArgs e)
+		{
+			string id = txt_DeptId.Text;
+			string command = "select * from tbl_Departments where DeptId = " + id;
+			cmd = new SqlCommand(command, con);
+			con.Open();
+			SqlDataReader dr = cmd.ExecuteReader();
+			if (dr.Read())
+			{
+				txt_DeptName.Text = dr[1].ToString();
+				txt_LocationId.Text = dr["LocId"].ToString();
+			}
+			else
+			{
+				Response.Write("Cannot find the record...", "Message");
+				Reset();
+			}
+			dr.Close();
+			con.Close();
+		}
+
+		private void btn_Update_Click(object sender, EventArgs e)
+		{
+			string id = txt_DeptId.Text;
+			string name = txt_DeptName.Text;
+			string loc = txt_LocationId.Text;
+
+			string command = string.Format("update tbl_Departments set DeptName = '{0}', LocId ={1} where DeptId = {2}", name, loc, id);
+
+			cmd = new SqlCommand(command, con);
+			con.Open();
+			cmd.ExecuteNonQuery();
+			con.Close();
+			Response.Write("Record updated...", "Message");
+			Reset();
+		}
+
+		private void btn_Delete_Click(object sender, EventArgs e)
+		{
+			string id = txt_DeptId.Text;
+
+			string command = "Delete from tbl_Departments where DeptId = " + id;
+
+			cmd = new SqlCommand(command, con);
+			con.Open();
+			cmd.ExecuteNonQuery();
+			con.Close();
+			Response.Write("Record Deleted...", "Message");
+			Reset();
+		}
+	}
+}
